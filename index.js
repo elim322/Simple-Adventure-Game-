c1eb6cb27602fc46b3ad193fc60099c0132e15fd
elim322 <einer.lim322@gmail.com>
2018-07-30 17:14:34 -0700

    Added basic adventure game

---
+++ index.js
@@ -0,0 +1,33 @@
+var message = prompt('You are a knight seeking adventure. You take your horse, Jericho, into the nearest forest. You reach a branched path. Will you go left or right?');
+if (message.toLowerCase() === 'left') {
+  var message = prompt('Oh no a monster appeared! Would you like to fight, or run?')
+  if (message.toLowerCase() ==='run')
+    alert('You are a crippling pussy!');
+    else if (message.toLowerCase() === 'fight')
+    var message = prompt('Do you dance, show cat pictures, stab it with a knife, or punch it in the face?');
+    if (message.toLowerCase() === 'dance')
+    var message = alert('You get totally destroyed in the dance battle!');
+    if (message.toLowerCase() === 'show cat pictures')
+    var message = alert('It has a cuter cat at home. You lose!');
+    if (message.toLowerCase() === 'stab it with a knife')
+    var message = alert('You go back to the castle and crowned as a hero!');
+    if (message.toLowerCase() === 'punch it in the face')
+    var message = alert('You go back to the castle and crowned as a hero!');
+}; 
+if (message.toLowerCase() === 'right'){
+  var message = prompt('you meet a pretty princess! Would you like to run, or take her back to the castle? ;)')
+  if (message.toLowerCase() === 'run'){
+    var message = alert('aww dude you really missed out on some tail! You lose!')
+  };
+  if (message.toLowerCase() === 'take her back to the castle') {
+    var message = prompt('Great! Now would you like to get freaky or respect women?')
+  }
+  if (message.toLowerCase() === 'get freaky') 
+    var message = alert('Looks like HE had a little extra below the belt. You lose!')
+    else if (message.toLowerCase() === 'respect women') {
+      var message = alert('Thats the way bro. You win!')
+    }
+};
+
+
+
