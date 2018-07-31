d969ac0d774244e4438a37a8d3592bed60154f3f
elim322 <einer.lim322@gmail.com>
2018-07-30 21:29:49 -0700

    fixed answers and added click to start button

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 //adventure game 
-var message = prompt('You are a knight seeking adventure. You take your horse, Jericho, into the nearest forest. You reach a branched path. Will you go left or right?');
+var message = prompt('You are Emilio, a knight seeking adventure. You take your horse, Jericho, into the nearest forest. You reach a branched path. Will you go left or right?');
 if (message.toLowerCase() === 'left') {
   var message = prompt('Oh no a monster appeared! Would you like to fight, or run?')
   if (message.toLowerCase() ==='run')
     alert('You are a crippling pussy!');
     else if (message.toLowerCase() === 'fight')
     var message = prompt('Do you dance, show cat pictures, stab it with a knife, or punch it in the face?');
-    if (message.toLowerCase() === 'dance')
+    if (message.includes('dance'))
     var message = alert('You get totally destroyed in the dance battle!');
-    if (message.toLowerCase() === 'show cat pictures')
+    if (message.includes('cat pictures'))
     var message = alert('It has a cuter cat at home. You lose!');
-    if (message.toLowerCase() === 'stab it with a knife')
+    if (message.includes('knife'))
     var message = alert('You go back to the castle and crowned as a hero!');
-    if (message.toLowerCase() === 'punch it in the face')
+    if (message.includes('punch'))
     var message = alert('You go back to the castle and crowned as a hero!');
 }; 
 if (message.toLowerCase() === 'right'){
@@ -20,12 +20,12 @@ if (message.toLowerCase() === 'right'){
   if (message.toLowerCase() === 'run'){
     var message = alert('aww dude you really missed out on some tail! You lose!')
   };
-  if (message.toLowerCase() === 'take her back to the castle') {
+  if (message.includes('back')) {
     var message = prompt('Great! Now would you like to get freaky or respect women?')
   }
-  if (message.toLowerCase() === 'get freaky') 
+  if (message.includes('freaky')) 
     var message = alert('Looks like HE had a little extra below the belt. You lose!')
-    else if (message.toLowerCase() === 'respect women') {
+    else if (message.includes('women')) {
       var message = alert('Thats the way bro. You win!')
     }
 };
